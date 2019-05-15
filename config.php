057562a5d01768c8ea2d14d0f1c537a0929b0b70
TJS-95 <TJS_95@163.com>
2019-05-15 09:10:51 +0800

    项目完成

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php 
+header("Content-type:text/html;charset=utf-8");
+
+/**
+ * 我们项目中用到的配置信息
+ */
+
+/**
+ * 数据库主机
+ */
+define('BX_DB_HOST', 'localhost');
+/**
+ * 数据库用户名
+ */
+define('BX_DB_USER', 'root');
+/**
+ * 数据库密码
+ */
+define('BX_DB_PASS', '123456');
+/**
+ * 数据库名字
+ */
+define('BX_DB_NAME', 'baixiu');
+
+/**
+ * F:\web前端\黑马\百秀项目\config.php
+ * F:\web前端\黑马\百秀项目\
+ */
+define('DIR_ROOT', dirname(__FILE__));
